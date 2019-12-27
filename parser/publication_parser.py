6f5c717f3ae6a95a6cfdcdbbd6982731919f69df
Pomin Wu <pomin5@gmail.com>
2019-12-27 11:00:47 +0800

    feat: parse metadata
    - parse published_at from JSON-LD

--- parser/publication_parser.py
+++ parser/publication_parser.py
@@ -8,6 +8,7 @@ import json
 
 import logging
 import readability
+import datetime
 
 readability.readability.log.setLevel(logging.ERROR)
 
@@ -20,7 +21,6 @@ def transform_snapshot(sn):
     doc = Document(sn["raw_data"])
 
     try:
-        # title
         title = doc.title()
 
         # main body
@@ -33,14 +33,91 @@ def transform_snapshot(sn):
         # article text
         text = "\n".join([" ".join(x.text.split()) for x in soup.find_all("p")])
 
+        # meta tag
+        soup = BeautifulSoup(sn["raw_data"], "html.parser")
+        # meta property
+        meta_property = {
+            x["property"]: x["content"]
+            for x in soup.find_all(
+                lambda tag: tag.name == "meta"
+                and "property" in tag.attrs.keys()
+                and "content" in tag.attrs.keys()
+            )
+        }
+        meta_name = {
+            x["name"]: x["content"]
+            for x in soup.find_all(
+                lambda tag: tag.name == "meta"
+                and "name" in tag.attrs.keys()
+                and "content" in tag.attrs.keys()
+            )
+        }
+        meta_itemprop = {
+            x["itemprop"]: x["content"]
+            for x in soup.find_all(
+                lambda tag: tag.name == "meta"
+                and "itemprop" in tag.attrs.keys()
+                and "content" in tag.attrs.keys()
+            )
+        }
+
+        # json ld
+        try:
+            jsonld = json.loads(
+                soup.find("script", {"type": "application/ld+json"}).text
+            )
+        except AttributeError:
+            jsonld = {}
+
+        # content
+        doc = Document(sn["raw_data"])
+        # title
+        title = doc.title()
+
+        # main body
+        s = doc.summary()
+        soup = BeautifulSoup(s, "html.parser")
+
+        # article text
+        text = " ".join([" ".join(x.text.split()) for x in soup.find_all("p")])
+
         # links
         external_links = [x["href"] for x in soup.find_all("a", href=lambda x: x)]
-        image_links = [x["data-src"] if "data-src" in x else x["src"] for x in soup.find_all("img")]
+        image_links = [
+            x.get("data-src", x.get("src", x.get("data-original", "")))
+            for x in soup.find_all("img")
+        ]
+
+        published_at = None
+
+        def parse_published_at(jsonld):
+            if "@type" in jsonld and (
+                jsonld["@type"] == "NewsArticle" or jsonld["@type"] == "Article"
+            ):
+                if "datePublished" in jsonld:
+                    return datetime.datetime.fromisoformat(
+                        jsonld["datePublished"]
+                    ).timestamp()
+            return None
+
+        def lookup_published_at(items):
+            for item in items:
+                r = parse_published_at(item)
+                if r is not None:
+                    return r
+
+        if isinstance(jsonld, list):
+            published_at = lookup_published_at(jsonld)
+        elif "@type" in jsonld:
+            published_at = parse_published_at(jsonld)
+        elif "@graph" in jsonld:
+            published_at = lookup_published_at(jsonld["@graph"])
 
         return {
             "publication_id": sn["article_id"],
             "producer_id": sn["site_id"],
             "canonical_url": sn["url"],
+            "published_at": published_at,
             "first_seen_at": sn["first_seen_at"],
             "last_updated_at": sn["last_updated_at"],
             "title": title,
@@ -50,6 +127,12 @@ def transform_snapshot(sn):
             "hashtags": "[]",
             "keywords": "[]",
             "tags": "[]",
+            "metadata": json.dumps(
+                {
+                    "meta": {**meta_itemprop, **meta_property, **meta_name},
+                    "json-ld": jsonld,
+                }
+            ),
         }
     except Exception as e:
         logging.error(e)
--- queries/parser/upsert_publication.sql
+++ queries/parser/upsert_publication.sql
@@ -1,7 +1,7 @@
 -- :name upsert_publication :insert
 INSERT INTO publication
-  (publication_id, producer_id, canonical_url, title, publication_text, first_seen_at, last_updated_at, urls, hashtags, keywords, tags)
+  (publication_id, producer_id, canonical_url, title, publication_text, published_at, first_seen_at, last_updated_at, urls, hashtags, keywords, tags, metadata)
 VALUES
-  (:publication_id, :producer_id, :canonical_url, :title, :publication_text, :first_seen_at, :last_updated_at, :urls, :hashtags, :keywords, :tags)
+  (:publication_id, :producer_id, :canonical_url, :title, :publication_text, :published_at, :first_seen_at, :last_updated_at, :urls, :hashtags, :keywords, :tags, :metadata)
 ON DUPLICATE KEY UPDATE
-  canonical_url = :canonical_url, title = :title, publication_text = :publication_text, first_seen_at = :first_seen_at, last_updated_at = :last_updated_at, urls = :urls, hashtags = :hashtags, keywords = :keywords, tags = :tags
+  canonical_url = :canonical_url, title = :title, publication_text = :publication_text, published_at = :published_at, first_seen_at = :first_seen_at, last_updated_at = :last_updated_at, urls = :urls, hashtags = :hashtags, keywords = :keywords, tags = :tags, metadata = :metadata
