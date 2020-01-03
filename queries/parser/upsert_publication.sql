96ae4fca2c40155a30f57f2b317441a197e0b4b4
Pomin Wu <pomin5@gmail.com>
2020-01-03 15:59:46 +0800

    feat: write parser mappings to db

--- queries/parser/upsert_publication.sql
+++ queries/parser/upsert_publication.sql
@@ -4,4 +4,4 @@ INSERT INTO publication
 VALUES
   (:publication_id, :producer_id, :canonical_url, :title, :publication_text, :published_at, :first_seen_at, :last_updated_at, :urls, :hashtags, :keywords, :tags, :metadata)
 ON DUPLICATE KEY UPDATE
-  canonical_url = :canonical_url, title = :title, publication_text = :publication_text, published_at = :published_at, first_seen_at = :first_seen_at, last_updated_at = :last_updated_at, urls = :urls, hashtags = :hashtags, keywords = :keywords, tags = :tags, metadata = :metadata
+  publication_id = LAST_INSERT_ID(publication_id), canonical_url = :canonical_url, title = :title, publication_text = :publication_text, published_at = :published_at, first_seen_at = :first_seen_at, last_updated_at = :last_updated_at, urls = :urls, hashtags = :hashtags, keywords = :keywords, tags = :tags, metadata = :metadata
