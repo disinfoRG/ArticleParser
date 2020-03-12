4fe7aed4bf54d720f3ec427f50a232a3c9196e40
Pomin Wu <pomin5@gmail.com>
2020-03-12 13:12:06 +0800

    feat: add publication version

--- queries/parser/upsert_publication.sql
+++ queries/parser/upsert_publication.sql
@@ -1,7 +1,7 @@
 -- :name upsert_publication :insert
 INSERT INTO publication
-  (publication_id, producer_id, canonical_url, title, publication_text, published_at, first_seen_at, last_updated_at, urls, hashtags, keywords, tags, metadata, comments)
+  (publication_id, producer_id, canonical_url, title, publication_text, published_at, first_seen_at, last_updated_at, urls, hashtags, keywords, tags, metadata, comments, version)
 VALUES
-  (:publication_id, :producer_id, :canonical_url, :title, :publication_text, :published_at, :first_seen_at, :last_updated_at, :urls, :hashtags, :keywords, :tags, :metadata, :comments)
+  (:publication_id, :producer_id, :canonical_url, :title, :publication_text, :published_at, :first_seen_at, :last_updated_at, :urls, :hashtags, :keywords, :tags, :metadata, :comments, :last_updated_at)
 ON DUPLICATE KEY UPDATE
-  publication_id = LAST_INSERT_ID(publication_id), canonical_url = :canonical_url, title = :title, publication_text = :publication_text, published_at = :published_at, first_seen_at = :first_seen_at, last_updated_at = :last_updated_at, urls = :urls, hashtags = :hashtags, keywords = :keywords, tags = :tags, metadata = :metadata, comments = :comments
+  publication_id = LAST_INSERT_ID(publication_id), canonical_url = :canonical_url, title = :title, publication_text = :publication_text, published_at = :published_at, first_seen_at = :first_seen_at, last_updated_at = :last_updated_at, urls = :urls, hashtags = :hashtags, keywords = :keywords, tags = :tags, metadata = :metadata, comments = :comments, version = :last_updated_at
