881ce2abd4603f2a40f1c6dcfff4339838d9974e
Pomin Wu <pomin5@gmail.com>
2020-03-12 11:28:39 +0800

    feat: add identifiers field

--- queries/parser/upsert_producer.sql
+++ queries/parser/upsert_producer.sql
@@ -1,7 +1,7 @@
 -- :name upsert_producer :insert
 INSERT INTO producer
-  (producer_id, name, classification, url, languages, licenses, followership)
+  (producer_id, name, classification, url, languages, licenses, followership, identifiers)
 VALUES
-  (:producer_id, :name, :classification, :url, :languages, :licenses, :followership)
+  (:producer_id, :name, :classification, :url, :languages, :licenses, :followership, :identifiers)
 ON DUPLICATE KEY UPDATE
-  producer_id = LAST_INSERT_ID(producer_id), name = :name, classification = :classification, url = :url
+  producer_id = LAST_INSERT_ID(producer_id), name = :name, classification = :classification, url = :url, identifiers = :identifiers
