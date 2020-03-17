8dacce112ee71c74423f274c953f4434a4dd8c1b
Pomin Wu <pomin5@gmail.com>
2020-03-17 10:33:30 +0800

    fix: error log info

--- parser/runner.py
+++ parser/runner.py
@@ -52,7 +52,8 @@ def process_each(items, data_saver, processor):
             item = processor(original)
             data_saver.save(Item(item=item, original=original))
         except Exception as e:
-            logger.error(traceback.format_exc(e))
+            logger.error(f"error processing item {item['publication_id']}")
+            logger.error(traceback.format_stack())
 
 
 def run_parser(processor, data_getter, data_saver, batch_size=1000, limit=10000):
