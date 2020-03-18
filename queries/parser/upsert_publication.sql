74f82be559e6906a00b31da5eebdd279e8b85069
Pomin Wu <pomin5@gmail.com>
2020-03-18 18:01:43 +0800

    fix: upsert publication id update clauses

--- queries/parser/upsert_publication.sql
+++ queries/parser/upsert_publication.sql
@@ -4,4 +4,4 @@ INSERT INTO publication
 VALUES
   (:publication_id, :producer_id, :canonical_url, :title, :publication_text, :published_at, :first_seen_at, :last_updated_at, :urls, :hashtags, :keywords, :tags, :metadata, :comments, :last_updated_at, :author, :connect_from)
 ON DUPLICATE KEY UPDATE
-  publication_id = LAST_INSERT_ID(publication_id), canonical_url = :canonical_url, title = :title, publication_text = :publication_text, published_at = :published_at, first_seen_at = :first_seen_at, last_updated_at = :last_updated_at, urls = :urls, hashtags = :hashtags, keywords = :keywords, tags = :tags, metadata = :metadata, comments = :comments, version = :last_updated_at, author = :author, connect_from = :connect_from
+  canonical_url = :canonical_url, title = :title, publication_text = :publication_text, published_at = :published_at, first_seen_at = :first_seen_at, last_updated_at = :last_updated_at, urls = :urls, hashtags = :hashtags, keywords = :keywords, tags = :tags, metadata = :metadata, comments = :comments, author = :author, connect_from = :connect_from
