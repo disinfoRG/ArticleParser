942f8ae48f8179c7c0cd02dea493ff6a3e9d25ad
Pomin Wu <pomin5@gmail.com>
2020-03-24 12:13:35 +0800

    fix: logger use

--- publisher/runner.py
+++ publisher/runner.py
@@ -1,16 +1,19 @@
 import logging
+import traceback
+
+logger = logging.getLogger(__name__)
 
 
 def processor(items, write, transformer):
     try:
         transformed = transformer(items)
     except Exception as e:
-        logging.error(e)
+        logger.error(e)
     else:
         try:
             write(list(transformed))
         except Exception as e:
-            logging.error(e)
+            logger.error(traceback.format_exc())
 
 
 def runner(from_db, getter, writer, transformer, batch_size=100):
@@ -20,7 +23,7 @@ def runner(from_db, getter, writer, transformer, batch_size=100):
         items = list(getter(from_db, offset=offset, limit=limit))
         if len(items) == 0:
             break
-        logging.debug(f"processing items {offset} to {offset + limit}")
+        logger.debug(f"processing items {offset} to {offset + limit}")
         processor(items=items, write=writer.write, transformer=transformer)
         offset += limit
     writer.close()
