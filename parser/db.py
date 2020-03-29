7786512b2ac1f573bb4337073d31980944a07ec5
Pomin Wu <pomin5@gmail.com>
2020-03-29 15:47:05 +0800

    refactor: remove unused scraper db queries

--- parser/db.py
+++ parser/db.py
@@ -2,12 +2,6 @@ import pugsql
 import os
 
 
-def scraper():
-    db = pugsql.module("queries/scraper")
-    db.connect(os.getenv("SCRAPER_DB_URL"))
-    return db
-
-
 def parser():
     db = pugsql.module("queries/parser")
     db.connect(os.getenv("DB_URL"))
--- queries/scraper/get_article_snapshots_by_site.sql
+++
@@ -1,8 +0,0 @@
--- :name get_article_snapshots_by_site :many
-SELECT
-  A1.article_id, A1.snapshot_at
-FROM ArticleSnapshot AS A1
-JOIN Article AS A2 ON A1.article_id = A2.article_id
-WHERE A2.site_id = :site_id
-LIMIT :limit
-OFFSET :offset
