a8067714f923fe9d7b548cb7f8b720dbec0d0024
Pomin Wu <pomin5@gmail.com>
2020-04-23 13:11:46 +0800

    chore: add m2 tasks

--- tasks.py
+++ tasks.py
@@ -1,3 +1,7 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+import os
 from invoke import task
 from sys import platform
 
@@ -10,3 +14,23 @@ def check(c):
 @task
 def test(c):
     c.run("python -m unittest")
+
+
+@task
+def migrate(c):
+    c.run("alembic upgrade head")
+
+
+@task
+def m2sh(c):
+    c.run(os.getenv("M2_SHELL"), pty=True)
+
+
+@task
+def m2(c):
+    c.run(f"python -m webbrowser -t '{os.getenv('M2_URL')}'")
+
+
+@task
+def tunnel(c):
+    c.run(os.getenv("M2_TUNNEL"))
