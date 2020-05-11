4383a60059df663f215d99d271ad84e1c8b9dd9f
Pomin Wu <pomin5@gmail.com>
2020-05-11 21:05:03 +0800

    feat: upload google drive public datasets

--- scripts/upload-google-drive.sh
+++ scripts/upload-google-drive.sh
@@ -4,5 +4,6 @@ PROCESSED_AT=$(date --date=$DATE '+%Y-%m-%d')
 
 for PRODUCER_ID in $(./ap-producer.py list | tail -n +3 | sed 's/  .*//'); do
   ./ap-datapublish.py publication --full-text --producer ${PRODUCER_ID} --processed-at ${PROCESSED_AT} --drive fulltext --service-account service.json
+  ./ap-datapublish.py publication --producer ${PRODUCER_ID} --processed-at ${PROCESSED_AT} --drive public --service-account service.json
 done
 
