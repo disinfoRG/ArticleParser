c316ac3cb61315f079e789304890d6b1f99e8500
Pomin Wu <pomin5@gmail.com>
2020-05-19 11:33:29 +0800

    chore: setup pytest and editable import

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="articleparser",
+    version="1.0",
+    description="Article parser for 0archive.tw",
+    author="Pomin Wu",
+    author_email="pomin5@gmail.com",
+    url="https://github.com/disinfoRG/ArticleParser/",
+    packages=["articleparser"],
+)
