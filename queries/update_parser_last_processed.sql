b773a625857cdd029afa7007d24de30420cadfe9
Pomin Wu <pomin5@gmail.com>
2020-05-28 13:39:46 +0800

    fix: revert e6e2d5

--- queries/update_parser_last_processed.sql
+++ queries/update_parser_last_processed.sql
@@ -1,7 +1,8 @@
 -- :name update_parser_last_processed
 UPDATE parser_info
 SET info = JSON_SET(
-  JSON_SET(info, "$.last_processed_article_id", :last_processed_article_id),
-  "$.last_processed_snapshot_at", :last_processed_snapshot_at)
+  JSON_SET(info,
+    "$.last_processed_article_id", GREATEST(JSON_EXTRACT(info, "$.last_processed_article_id"), :last_processed_article_id)),
+    "$.last_processed_snapshot_at", GREATEST(JSON_EXTRACT(info, "$.last_processed_snapshot_at"), :last_processed_snapshot_at))
 WHERE parser_name = :parser_name
 
