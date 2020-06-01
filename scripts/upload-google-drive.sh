87a6305d95fef4b4006a2e80691d3f242d3fcc20
Pomin Wu <pomin5@gmail.com>
2020-06-01 18:00:41 +0800

    fix: extend upload script published at argument

--- scripts/upload-google-drive.sh
+++ scripts/upload-google-drive.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-DATE=${1:-"yesterday"}
-PROCESSED_AT=$(date --date=$DATE '+%Y-%m-%d')
+PROCESSED_AT=${1:-$(date --date="yesterday" '+%Y-%m-%d')}
 
 for PRODUCER_ID in $(./ap-producer.py list | tail -n +3 | sed 's/  .*//'); do
   ./ap-datapublish.py publication --full-text --producer ${PRODUCER_ID} --processed-at ${PROCESSED_AT} --drive fulltext --service-account service.json
